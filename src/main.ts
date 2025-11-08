65b241e65607b82e935f905b756ff0152b403578
lemoncmd <lemoncmd@gmail.com>
2025-11-08 19:17:08 +0900

    実行機能を加へ候

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import * as fs from "fs";
 import { argv } from "process";
 import { compile } from "./lib";
+import requireFromString from "require-from-string";
 
 const input = fs.readFileSync(argv[2], "utf-8");
-console.log(compile(input));
+if (argv[3] === "--run") {
+  const module = requireFromString(compile(input));
+  module.本丸();
+} else {
+  console.log(compile(input));
+}
